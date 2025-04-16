cc0450799e6837423a96e5b6ed761f436dcc2116
Moussa-19 <exemplegreat246@gmail.com>
2025-04-16 10:41:54 +0100

    Ajout du nom du développeur BAMMOU MOUSSA

--- devoir.c
+++ devoir.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    printf("SCRUM master");
+    printf("SCRUM master: ROFI EL MEHDI`\n ");
+    printf("Développeur : Bammou Moussa\n");
+    
 }
