eb9847cea17b6f9aaf9d0ee46435a4e917041f78
Moussa-19 <exemplegreat246@gmail.com>
2025-04-18 15:46:37 +0100

    Rôle : Développeur

--- devoir.c
+++ devoir.c
@@ -4,6 +4,7 @@ int main(){
     printf("SCRUM master: ROFI EL MEHDI`\n ");
     printf("Développeur : Bammou Moussa\n");
     printf("Projet : Atelier Git\n");
+    printf("Rôle : Développeur\n");
    
 
     
